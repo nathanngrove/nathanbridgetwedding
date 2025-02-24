2770d09e012239d234cea20088abb7183a6962ce
Nathan Grove <nathangrove08@gmail.com>
2025-02-23 19:13:19 -0800

    testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	assetPrefix: "/nathanbridgetwedding",
+	basePath: "/nathanbridgetwedding",
 	output: "export",
 	reactStrictMode: true,
 };
