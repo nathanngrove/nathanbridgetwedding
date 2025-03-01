0a73aa0492053b7158115feffe9fdb3330445f1c
Nathan Grove <nathangrove08@gmail.com>
2025-02-28 20:05:33 -0800

    testing

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
 	basePath: "/nathanbridgetwedding",
 	output: "export",
 	images: { unoptimized: true },
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
